83a34721402bdedc978a2e77657699c8d59df9d3
barbosalopes <30329375+barbosalopes@users.noreply.github.com>
2019-03-16 12:37:36 -0300

    Add files via upload

---
+++ DoubleReferencedQueue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication6.Item;
+
+namespace ConsoleApplication6.Methods
+{
+    public class DoubleReferencedQueue <Type>
+    {
+        private DoubleReferencedItem<Type> First, Last;
+
+        public DoubleReferencedQueue(Type value)
+        {
+
+        }
+
+    }
+}
---
+++ QueueItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6.Item
+{
+    public class QueueItem <Type> : Item <Type>
+    {
+        public QueueItem<Type> Next { set; get; }
+        
+        public QueueItem(Type data) : base(data)
+        {
+            Next = null;
+        }
+
+    }
+}
